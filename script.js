7805a65146baa79f446ec4b7b000a3132b2d9e68
bkrmroy <110381880+bkrmroy@users.noreply.github.com>
2023-07-05 17:48:34 +0600

    create the html and css

---
+++ script.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+function Book(title, author , pages, readStaus) {
+  // the constructor...
+  title = this.title;
+  author = this.author;
+  pages =this.pages;
+  readStaus= this.readStaus
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
