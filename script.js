a5cf2864e10126626b854b62f62273077208657c
bkrmroy <110381880+bkrmroy@users.noreply.github.com>
2023-07-05 18:34:06 +0600

    write constructor function for a book

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+const bookContainer = document.querySelector(".book-container")
+
 let myLibrary = [];
 
 function Book(title, author , pages, readStaus) {
-  // the constructor...
-  title = this.title;
-  author = this.author;
-  pages =this.pages;
-  readStaus= this.readStaus
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readStaus = readStaus;
 }
+const book1= new Book('X','a','55', 'read')
+const book2= new Book('Y','a','51', 'not read yet')
 
 function addBookToLibrary() {
-  // do stuff here
+  myLibrary.push(book1)
+  myLibrary.push(book2)
 }
+addBookToLibrary()
+
