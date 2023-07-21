7f56a71f23836e018fc9d69b78f16247d18cbc11
bkrmroy <110381880+bkrmroy@users.noreply.github.com>
2023-07-21 10:30:24 +0600

    add feature to change read status

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ const btnFormClose = document.querySelector(".form-close")
 const overlay = document.querySelector(".overlay")
 const btnFormSubmit = document.querySelector(".btn-form-submit");
 const form = document.querySelector(".main-form")
+const infoContainer = document.querySelector(".info-container")
+const totalBooks = document.querySelector(".total-books")
+const totalPages = document.querySelector(".total-pages")
 let myLibrary = [];
 
-function Book(title, author , pages) {
+function Book(title, author , pages, readStatus) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  // this.readStaus = readStaus;
+  this.readStatus = readStatus;
 }
-
 function addBookToLibrary(event) {
   event.preventDefault()
   const title = document.getElementById("new-book-title").value;
@@ -32,23 +34,21 @@ function addBookToLibrary(event) {
   })
   if(bookExistsInArray === true) return
   else{
-    const book = new Book(title, author, pages)
+    const book = new Book(title, author, pages, 'Not read yet')
     myLibrary.push(book)
     loopThroughLibrary()
     console.log(myLibrary)
 
     const form = document.querySelector(".form.active")
     closeForm(form);
+    updateInfo()
   }
 }
-
 btnFormSubmit.addEventListener("click",()=>{
   form.checkValidity();
   form.reportValidity();
 })
 btnFormSubmit.addEventListener("click",addBookToLibrary)
-
-
 
 function loopThroughLibrary(){
   myLibrary.forEach(item => {
@@ -68,7 +68,7 @@ function loopThroughLibrary(){
     const book = document.createElement("div")
     book.classList.add("book")
     
-    // book.dataset.number = myLibrary.indexOf(item)
+    book.dataset.number = myLibrary.indexOf(item)
 
     const bookTitle = document.createElement("div")
     bookTitle.classList.add("book-title")
@@ -85,35 +85,66 @@ function loopThroughLibrary(){
     bookPages.textContent = `${item.pages} pages`
     book.appendChild(bookPages)
 
+    const readStatus = document.createElement("div")
+    readStatus.classList.add("book-read-status")
+    readStatus.textContent = `Status: ${item.readStatus}`
+    book.appendChild(readStatus)
 
-    // const readStaus = document.createElement("div")
-    // readStaus.classList.add("book-read-status")
-    // readStaus.textContent = `Status: ${item.readStaus}`
-    // book.appendChild(readStaus)
-
-    // bookContainer.appendChild(book);
-    // bookContainer.insertBefore(book, bookContainer.firstChild.nextSibling);
+    const changeReadStatus= document.createElement("button")
+    changeReadStatus.classList.add("change-read-status")
+    changeReadStatus.textContent = 'Change read status'
+    book.appendChild(changeReadStatus)
 
     const removeBook = document.createElement("button")
     removeBook.classList.add("remove-book")
     removeBook.textContent = 'Remove'
     book.appendChild(removeBook)
-    
 
     btnAddBook.insertAdjacentElement('afterend', book)
 })
 }
-
+function setDataNum(){
+  const domBooklist = document.querySelectorAll(".book")
+  myLibrary.forEach(item =>{
+    domBooklist.forEach(book =>{
+      if(book.childNodes[0].textContent === item.title &&
+        book.childNodes[1].textContent === item.author &&
+        book.childNodes[2].textContent === `${item.pages} pages`){
+          book.dataset.number = myLibrary.indexOf(item)
+        }
+    })
+  })
+}
 //remove book
 bookContainer.addEventListener('click',function(event){
   if(event.target.classList.contains('remove-book')){
     event.target.parentElement.remove()
-    myLibrary.splice(myLibrary.indexOf(event.target.parentElement), 1)
-    console.log(myLibrary)
+    let index = event.target.parentElement.dataset.number
+    myLibrary.splice(index, 1)
+    setDataNum()
+    updateInfo()
   }
 })
+//change read status
+bookContainer.addEventListener('click',function(event){
+  if(event.target.classList.contains('change-read-status')){
+    const bookReadStatus = event.target.parentElement.querySelector('.book-read-status')
+    let index = event.target.parentElement.dataset.number
 
-
+    if(bookReadStatus.textContent === 'Status: Not read yet'){
+      bookReadStatus.textContent = 'Status: Read'
+      myLibrary[index].readStatus = 'Read'
+      console.log(myLibrary[index].readStatus)
+      return
+    }
+    if(bookReadStatus.textContent === 'Status: Read'){
+      bookReadStatus.textContent = 'Status: Not read yet'
+      myLibrary[index].readStatus = 'Not read yet'
+      console.log(myLibrary[index].readStatus)
+      return
+    }
+  }
+})
 //popup form
 btnAddBook.addEventListener("click", ()=>{
   const form = document.querySelector(".form")
@@ -137,4 +168,18 @@ function closeForm(form){
   form.classList.remove("active")
   overlay.classList.remove("active")
   document.querySelector(".main-form").reset()
+}
+//info Container
+let pagesNum;
+function countPages(){
+  myLibrary.forEach(item =>{
+    pagesNum += parseInt(item.pages)
+    return pagesNum
+  })
+}
+function updateInfo(){
+  pagesNum = 0
+  countPages()
+  totalBooks.textContent = `Total books: ${myLibrary.length}`
+  totalPages.textContent = `Total Pages: ${pagesNum}`
 }
